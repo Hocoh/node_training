d7f0c076c112e04888bc81ebd35a111d5632083d
J.Doe <sunning@protonmail.com>
2018-07-14 04:22:27 +0200

    commit

--- mongo/hashing.js
+++ mongo/hashing.js
@@ -1,5 +1,20 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt= require("bcryptjs"); 
+
+var password="123abc"; 
+
+bcrypt.genSalt(10, (err, salt) => { 
+    bcrypt.hash(password, salt, (err, hash) => { 
+      console.log("hash", hash); 
+    })
+});
+
+var hashedPassword= "$2a$10$5RwTnJ/fMJohxuAhR6S.B.VLp//9Nko3QOJViiOHhxskZkdToVlHy"
+
+bcrypt.compare("123", hashedPassword, (err, res) => {
+  console.log(res);
+});
 
 var data = {
   id: 10
